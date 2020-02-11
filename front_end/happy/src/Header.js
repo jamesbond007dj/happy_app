31b2b9743abef302560fff77ce8710060c31e3b9
jamesbond007dj <ilkerzaimoglu@gmail.com>
2020-02-11 08:34:47 -0800

    react started

---
+++ front_end/happy/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default props => (
+    <>
+        <h1>HAPPY HOUR APP</h1>
+        <ul>
+            <li>Login</li>
+            <li>About us</li>
+        </ul>
+        <p>Come-on Join Cool  Happy Hours : {props.count}</p>
+    <>
+)
+
+export default Header;
