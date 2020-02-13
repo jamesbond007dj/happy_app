6827c5583ee8471d09b397c4130a6a5d15d8b7e8
Ting Luo <tin831@gmail.com>
2020-02-12 20:13:52 -0800

    api change

--- back_end/happy/models.py
+++ back_end/happy/models.py
@@ -1,38 +1,58 @@
 from django.db import models
 from django.contrib.auth.models import User
+from datetime import datetime
+
+DAYS = [
+    ('mon', 'Monday'),
+    ('tue', 'Tuesday'),
+    ('wed', 'Wednesday'),
+    ('thu', 'Thursday'),
+    ('fri', 'Friday'),
+    ('sat', 'Saturday'),
+    ('sun', 'Sunday'),
+]
+MON = 'mon'
+TUE = 'tue'
+WED = 'wed'
+THU = 'thu'
+FRI = 'fri'
+SAT = 'sat'
+SUN = 'sun'
 
 class Entry(models.Model):
   author = models.ForeignKey(User, on_delete=models.CASCADE)
   title = models.CharField(max_length=50)
+  start_time = models.TimeField(null=True,blank=True)
+  end_time = models.TimeField(null=True,blank=True)
   image = models.ImageField(upload_to='images/',blank=True)
-  monday_start_time_1 = models.TimeField(null=True,blank=True)
-  monday_end_time_1 = models.TimeField(null=True,blank=True)
-  tuesday_start_time_1 = models.TimeField(null=True,blank=True)
-  tuesday_end_time_1 = models.TimeField(null=True,blank=True)
-  wednesday_start_time_1 = models.TimeField(null=True,blank=True)
-  wednesday_end_time_1 = models.TimeField(null=True,blank=True)
-  thursday_start_time_1 = models.TimeField(null=True,blank=True)
-  thursday_end_time_1 = models.TimeField(null=True,blank=True)
-  friday_start_time_1 = models.TimeField(null=True,blank=True)
-  friday_end_time_1 = models.TimeField(null=True,blank=True)
-  saturday_start_time_1 = models.TimeField(null=True,blank=True)
-  saturday_end_time_1 = models.TimeField(null=True,blank=True)
-  sunday_start_time_1 = models.TimeField(null=True,blank=True)
-  sunday_end_time_1 = models.TimeField(null=True,blank=True)
-  monday_start_time_2 = models.TimeField(null=True,blank=True)
-  monday_end_time_2 = models.TimeField(null=True,blank=True)
-  tuesday_start_time_2 = models.TimeField(null=True,blank=True)
-  tuesday_end_time_2 = models.TimeField(null=True,blank=True)
-  wednesday_start_time_2 = models.TimeField(null=True,blank=True)
-  wednesday_end_time_2 = models.TimeField(null=True,blank=True)
-  thursday_start_time_2 = models.TimeField(null=True,blank=True)
-  thursday_end_time_2 = models.TimeField(null=True,blank=True)
-  friday_start_time_2 = models.TimeField(null=True,blank=True)
-  friday_end_time_2 = models.TimeField(null=True,blank=True)
-  saturday_start_time_2 = models.TimeField(null=True,blank=True)
-  saturday_end_time_2 = models.TimeField(null=True,blank=True)
-  sunday_start_time_2 = models.TimeField(null=True,blank=True)
-  sunday_end_time_2 = models.TimeField(null=True,blank=True)
+  # monday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # monday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # tuesday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # tuesday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # wednesday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # wednesday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # thursday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # thursday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # friday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # friday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # saturday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # saturday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # sunday_start_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # sunday_end_time_1 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # monday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # monday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # tuesday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # tuesday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # wednesday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # wednesday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # thursday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # thursday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # friday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # friday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # saturday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # saturday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # sunday_start_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  # sunday_end_time_2 = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
   description = models.TextField(null=True,blank=True)
   menu = models.TextField(null=True,blank=True)
   address = models.CharField(max_length=120,null=True,blank=True)
@@ -41,3 +61,26 @@ class Entry(models.Model):
   
   def __str__(self):
     return self.title
+
+class StartTime(models.Model):
+  time = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  day_choice = [
+    (MON, 'Monday'),
+    (TUE, 'Tuesday'),
+    (WED, 'Wednesday'),
+    (THU, 'Thursday'),
+    (FRI, 'Friday'),
+    (SAT, 'Saturday'),
+    (SUN, 'Sunday'),
+]
+  day = models.CharField(
+      max_length=10,
+      choices=day_choice,
+      default=MON,
+  )
+  entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
+
+class EndTime(models.Model): 
+  time = models.TimeField(default=datetime.now().time().replace(hour=0, minute=0, second=0),null=True,blank=True)
+  day = models.CharField(max_length=10)
+  entry = models.ForeignKey(Entry, on_delete=models.CASCADE)
--- back_end/happy/views.py
+++ back_end/happy/views.py
@@ -1,5 +1,5 @@
 from rest_framework import generics
-from .models import Entry
+from .models import Entry, StartTime, EndTime
 from .serializers import EntrySerializer
 from .permissions import IsAuthorOrReadOnly
 from datetime import datetime
@@ -21,33 +21,43 @@ class AddDetailView(generics.RetrieveUpdateDestroyAPIView):
 
 class CurrentListView(generics.ListAPIView):
   serializer_class = EntrySerializer
-  def get_querysetxxx(self):
-    today = day_name[datetime.today().weekday()].lower()
-    ## monday equals 0, sunday equals 6
-    start_time_1 = today + '_start_time_1'
-    start_time_2 = today + '_start_time_2'
-    end_time_1 = today + '_end_time_1'
-    end_time_2 = today + '_end_time_2'
-    current_time = datetime.now().strftime('%X')
-    return Entry.objects.all().filter(start_time_1__lte=current_time).filter(end_time_1__gte=current_time)
+  # def get_querysetxxx(self):
+  #   today = day_name[datetime.today().weekday()].lower()
+  #   ## monday equals 0, sunday equals 6
+  #   start_time_1 = today + '_start_time_1'
+  #   start_time_2 = today + '_start_time_2'
+  #   end_time_1 = today + '_end_time_1'
+  #   end_time_2 = today + '_end_time_2'
+  #   current_time = datetime.now().strftime('%X')
+  #   return Entry.objects.all().filter(start_time_1__lte=current_time).filter(end_time_1__gte=current_time)
 
   def get_queryset(self):
     today = day_name[datetime.today().weekday()].lower()
     current = []
     for e in Entry.objects.all():
-      if is_current(e,today):
-        current.append(e)
+      start_times = StartTime.objects.filter(entry=e.id)
+      end_times = EndTime.objects.filter(entry=e.id)
+      times = zip(start_times, end_times)
+      thing = is_current(e,times)
+      if thing:
+        current.append((e, thing))
+        return 
 
     return current
 
-def is_current(e,today):
-  day_start_time_1 = today + '_start_time_1'
-  day_end_time_1 = today + '_end_time_1'
-  print('entrtyyyy', e.__dict__[day_start_time_1])
-  current_time = datetime.now().strftime('%X')
-  entry_start_time = e.__dict__[day_start_time_1].strftime('HH:mm:ss (%H:%M:%S)')
-  entry_end_time = str(e.__dict__[day_end_time_1])
-  if current_time > entry_start_time and current_time < entry_end_time:
-    return True
-  else:
-    return False
+def is_current(e,times):
+  # day_start_time_1 = today + '_start_time_1'
+  # day_end_time_1 = today + '_end_time_1'
+  # day_start_time_2 = today + '_start_time_2'
+  # day_end_time_2 = today + '_end_time_2'
+  current_time = datetime.now().time()
+  # entry_start_time_1 = e.__dict__[day_start_time_1]
+  # entry_end_time_1 = e.__dict__[day_end_time_1]
+  # entry_start_time_2 = e.__dict__[day_start_time_2]
+  # entry_end_time_2 = e.__dict__[day_end_time_2]
+  # print('hallllllo',e.__dict__)
+  for time_pair in times:
+    if current_time > time_pair[0].time and current_time < time_pair[1].time:
+      return True
+    else:
+      return False
