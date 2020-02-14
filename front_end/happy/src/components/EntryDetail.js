031469de292b6407372b6c18c44db1044b7e3467
jamesbond007dj <ilkerzaimoglu@gmail.com>
2020-02-13 18:14:02 -0800

    comment form

--- front_end/happy/src/components/EntryDetail.js
+++ front_end/happy/src/components/EntryDetail.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Link } from "react-router-dom"
 import EntryForm from './EntryForm'
+import EntryComment from './EntryComment'
 
 
 export default props => (
     <>
         {/* <Link to="/"><p>Go Home </p></Link> */}
         <EntryForm onSubmit={props.onSubmit} entry={props.entry} />
+        <EntryComment onSubmit={props.onSubmit} entry={props.entry} />
     </>
 )
